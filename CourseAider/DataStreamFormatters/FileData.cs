c6f6e36cbd0fde421af5e9e95eb469d7f148165d
bladespinner <bladespinner@gmail.com>
2015-02-05 22:14:21 +0200

    dasasdfasdfadsfadfdsaf

---
+++ CourseAider/DataStreamFormatters/FileData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CourseAider.DataStreamFormatters
+{
+    /// <summary>
+    /// Class to store attached file info
+    /// </summary>
+    public class FileData
+    {
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Data { get; set; }
+
+        public long Size { get { return (Data != null ? Data.LongLength : 0L); } }
+
+        /// <summary>
+        /// Create a FileData from HttpContent 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static async Task<FileData> ReadFile(HttpContent file)
+        {
+            var data = await file.ReadAsByteArrayAsync();
+            var result = new FileData()
+            {
+                FileName = FixFilename(file.Headers.ContentDisposition.FileName),
+                ContentType = file.Headers.ContentType.ToString(),
+                Data = data
+            };
+            return result;
+        }
+
+        /// <summary>
+        /// Amend filenames to remove surrounding quotes and remove path from IE
+        /// </summary>
+        /// <param name="original"></param>
+        /// <returns></returns>
+        private static string FixFilename(string original)
+        {
+            var result = original.Trim();
+            // remove leading and trailing quotes
+            if (result.StartsWith("\""))
+                result = result.TrimStart('"').TrimEnd('"');
+            // remove full path versions
+            if (result.Contains("\\"))
+                // parse out path
+                result = new System.IO.FileInfo(result).Name;
+
+            return result;
+        }
+    }
+}
---
+++ CourseAider/DataStreamFormatters/InMemoryMultipartFormDataStreamProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseAider.DataStreamFormatters
+{
+    public class CustomMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
+    {
+        public CustomMultipartFormDataStreamProvider(string path)
+            : base(path)
+        {
+
+        }
+
+        public override string GetLocalFileName(System.Net.Http.Headers.HttpContentHeaders headers)
+        {
+            string fileName;
+            if (!string.IsNullOrWhiteSpace(headers.ContentDisposition.FileName))
+            {
+                fileName = headers.ContentDisposition.FileName;
+            }
+            else
+            {
+                fileName = Guid.NewGuid().ToString() + ".data";
+            }
+            return fileName.Replace("\"", string.Empty);
+        }
+    }
+}
